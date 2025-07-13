272cb6f7a6878e5e25e923d346b06bfe5f21796a
Jeff97ares <92552303+Jeff97ares@users.noreply.github.com>
2025-07-13 17:45:26 -0500

    Delete backend directory

--- backend/src/routes/specialty.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/specialty.controller');
-
-router.post('/', controller.createSpecialty);
-router.get('/', controller.getAllSpecialties);
-
-module.exports = router;
